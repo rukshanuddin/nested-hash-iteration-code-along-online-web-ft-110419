629542ae3be571604a5ca332bf4703f824ccb64a
Rukshan Uddin <rukshan.uddin@gmail.com>
2019-11-07 21:53:38 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -17,7 +17,7 @@ require 'pry'
 def remove_strawberry(contacts)
   contacts.each do |person, contact_details_hash|
     if person == "Freddy Mercury"
-      contact_details_hash.each do |detail, attribute, data|
+      contact_details_hash.each do |detail, attribute|
         if attribute == :favorite_ice_cream_flavors
       binding.pry
     end
